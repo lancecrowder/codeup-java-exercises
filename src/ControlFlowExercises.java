d44eef502a2d91564e0d128d0f5329e8fc39368e
lancecrowder <thelancecrowder@gmail.com>
2018-04-30 16:46:09 -0500

    Control exercise created and completed step b-2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
-        while (i<=100){
+        int i = 100;
+        while (i>=-10){
             System.out.println(i);
-            i+=2;
+            i-=5;
         }
         }
     }
