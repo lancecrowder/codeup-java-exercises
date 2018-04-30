e609cabb06d443d7c778f4431431c7e51fc58485
lancecrowder <thelancecrowder@gmail.com>
2018-04-30 14:52:06 -0500

    Console I/O completed and Cleaned up

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,32 +3,34 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-//        int myInt;
-//        Scanner sc= new Scanner(System.in);
-//        System.out.println(("Please enter an integer :"));
-//        myInt = sc.nextInt();
-//        System.out.println(("You Entered " + myInt));
-//
-//        String stringOne;
-//        String stringTwo;
-//        String stringThree;
-//
-//        Scanner lc = new Scanner(System.in);
-//        System.out.println(("Please enter three words"));
-//        stringOne = lc.next();
-//        stringTwo = lc.next();
-//        stringThree = lc.nextLine();
-//
-//        System.out.println(("Your Primary word is: " + stringOne));
-//        System.out.println(("Your Secondary word is: " + stringTwo));
-//        System.out.println(("Your Tirtiary word is: " + stringThree));
-//
-//        String myString;
-//        System.out.println(("Please enter a sentence: "));
-//        Scanner jc = new Scanner(System.in);
-//        myString = jc.nextLine();
-//        System.out.println(myString);
+//        INTEGER EXERCISE
+        int myInt;
+        Scanner sc= new Scanner(System.in);
+        System.out.println(("Please enter an integer :"));
+        myInt = sc.nextInt();
+        System.out.println(("You Entered " + myInt));
+//          THREE WORDS EXERCISE
+        String stringOne;
+        String stringTwo;
+        String stringThree;
 
+        Scanner lc = new Scanner(System.in);
+        System.out.println(("Please enter three words"));
+
+        stringOne = lc.next();
+        stringTwo = lc.next();
+        stringThree = lc.nextLine();
+
+        System.out.println(("Your Primary word is: " + stringOne));
+        System.out.println(("Your Secondary word is: " + stringTwo));
+        System.out.println(("Your Tirtiary word is: " + stringThree));
+//          SENTENCE EXERCISE
+        String myString;
+        System.out.println(("Please enter a sentence: "));
+        Scanner jc = new Scanner(System.in).useDelimiter("\n");
+        myString = jc.nextLine();
+        System.out.println(myString);
+//          Area and Perimeter Exercise
         int length;
         int width;
         int area;
@@ -42,7 +44,5 @@ public class ConsoleExercises {
         perimeter = (2*length)+(2*width);
         System.out.println(("The area of the room is: " + area));
         System.out.println(("The Perimeter of the room is: " + perimeter));
-
-
     }
 }
