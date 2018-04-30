186b67a319443616e3d691157f43d09872b61579
lancecrowder <thelancecrowder@gmail.com>
2018-04-30 14:24:49 -0500

    Console I/O began and completed step 3

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,18 +9,24 @@ public class ConsoleExercises {
 //        myInt = sc.nextInt();
 //        System.out.println(("You Entered " + myInt));
 
-        String stringOne;
-        String stringTwo;
-        String stringThree;
+//        String stringOne;
+//        String stringTwo;
+//        String stringThree;
+//
+//        Scanner lc = new Scanner(System.in);
+//        System.out.println(("Please enter three words"));
+//        stringOne = lc.next();
+//        stringTwo = lc.next();
+//        stringThree = lc.nextLine();
+//
+//        System.out.println(("Your Primary word is: " + stringOne));
+//        System.out.println(("Your Secondary word is: " + stringTwo));
+//        System.out.println(("Your Tirtiary word is: " + stringThree));
 
-        Scanner lc = new Scanner(System.in);
-        System.out.println(("Please enter three words"));
-        stringOne = lc.next();
-        stringTwo = lc.next();
-        stringThree = lc.nextLine();
-
-        System.out.println(("Your Primary word is: " + stringOne));
-        System.out.println(("Your Secondary word is: " + stringTwo));
-        System.out.println(("Your Tirtiary word is: " + stringThree));
+        String myString;
+        System.out.println(("Please enter a sentence: "));
+        Scanner jc = new Scanner(System.in);
+        myString = jc.next();
+        System.out.println(myString);
     }
 }
