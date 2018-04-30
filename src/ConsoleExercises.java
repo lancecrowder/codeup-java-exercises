268dcc76fa1e1a7bdb0e5b46adbb67d665d78297
lancecrowder <thelancecrowder@gmail.com>
2018-04-30 14:19:17 -0500

    Console I/O began and completed step 2

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        int myInt;
-        Scanner sc= new Scanner(System.in);
-        System.out.println(("Please enter and integer :"));
-        myInt = sc.nextInt();
-        System.out.println(("You Entered " + myInt));
+//        int myInt;
+//        Scanner sc= new Scanner(System.in);
+//        System.out.println(("Please enter and integer :"));
+//        myInt = sc.nextInt();
+//        System.out.println(("You Entered " + myInt));
+
+        String stringOne;
+        String stringTwo;
+        String stringThree;
+
+        Scanner lc = new Scanner(System.in);
+        System.out.println(("Please enter three words"));
+        stringOne = lc.next();
+        stringTwo = lc.next();
+        stringThree = lc.nextLine();
+
+        System.out.println(("Your Primary word is: " + stringOne));
+        System.out.println(("Your Secondary word is: " + stringTwo));
+        System.out.println(("Your Tirtiary word is: " + stringThree));
     }
 }
