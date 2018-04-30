b065b53d23cdb90e888cc792d2e0a1e944ca3297
lancecrowder <thelancecrowder@gmail.com>
2018-04-30 11:52:44 -0500

    Java Syntax, Data Types, and Variables Exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -23,6 +23,20 @@ public class HelloWorld {
 //        int x = 5;
         System.out.println(++x);
         System.out.println(x);
+
+//        class class = unexpected token and identifier expected
+
+        int x = 4;
+        x += 5;
+
+        x = 3;
+        int y = 4;
+        y *= x;
+
+        x = 10;
+        y = 2;
+        x /= y;
+        y -= x;
     }
 }
 
