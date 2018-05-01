5f9b8d6b6250f6e02f0de000e8e094217d9b7470
lancecrowder <thelancecrowder@gmail.com>
2018-05-01 10:40:50 -0500

    Control exercise created and completed step Number grades to letter grades.  Done!!

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,20 +9,22 @@ public class ControlFlowExercises {
         do {
             double theirNumber;
             Scanner scan = new Scanner(System.in);
-            System.out.println("What number would you like to go up to? ");
+            System.out.println("Please enter your Number Grade: ");
             theirNumber = Double.parseDouble(scan.nextLine());
-            System.out.println("Here is your table!");
+            System.out.println("Here is your Letter Grade!");
             System.out.println();
-            System.out.println("number | squared | cubed ");
-            System.out.println("------ | ------  | ------ ");
-            for (int i = 0; i <= theirNumber; i++) {
-                if (i < 10) {
-                    System.out.println(i + "      | " + (Math.pow(i, 2)) + "     | " + (Math.pow(i, 3)));
-                } else if (i >= 10 && i < 100) {
-                    System.out.println(i + "     | " + (Math.pow(i, 2)) + "    | " + (Math.pow(i, 3)));
+                if (theirNumber >= 88) {
+                    System.out.println("A - Great Job!");
+                } else if (theirNumber <= 87 && theirNumber >= 80) {
+                    System.out.println("B - Not Bad!");
+                } else if (theirNumber <= 79 && theirNumber >= 67){
+                    System.out.println("C - C's get degrees!");
+                } else if (theirNumber <= 66 && theirNumber >= 60){
+                    System.out.println("D - Squeaking by!");
+                } else {
+                    System.out.println("F - You Suck...");
                 }
-            }
-            System.out.println("Would you like to enter a new number? Y or N");
+                    System.out.println("Would you like to enter a new Grade? Y or N");
             playAgain = letter.nextLine();
         } while (playAgain.equals(Y) || playAgain.equals(y));
     }
