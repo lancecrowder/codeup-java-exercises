1b4051450f18f5532e25fd12e742b4cc2ed96859
lancecrowder <thelancecrowder@gmail.com>
2018-05-01 10:10:22 -0500

    Control exercise created and completed step Table of Powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-//            System.out.println(i);
-            if (i%3 == 0 && i%5 == 0){
-                System.out.println(("FizzBuzz"));
-            }  else if (i%5 == 0){
-                System.out.println("Buzz");
-            } else if (i%3 == 0){
-                System.out.println("Fizz");
-            }  else {
-                System.out.println(i);
+        double theirNumber;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What number would you like to go up to? ");
+        theirNumber = Double.parseDouble(scan.nextLine());
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("number | squared | cubed ");
+        System.out.println("------ | ------  | ------ ");
+        for (int i=0; i <= theirNumber; i++) {
+            if (i<10) {
+                System.out.println(i + "      | " + (Math.pow(i, 2)) + "     | " + (Math.pow(i, 3)));
+            } else if (i>=10 && i<100) {
+                System.out.println(i + "     | " + (Math.pow(i, 2)) + "    | " + (Math.pow(i, 3)));
             }
         }
     }
