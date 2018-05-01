3fc40f14d74fddb0f890d5ea5c5f6bbdfc617e9f
lancecrowder <thelancecrowder@gmail.com>
2018-05-01 09:27:42 -0500

    Control exercise created and completed step c-b-3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,6 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 100; i >= -10; i-=5) {
+        for (double i = 2; i <= 1000000; i=Math.pow(i, 2)) {
             System.out.println(i);
         }
     }
