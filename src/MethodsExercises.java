2971449bd908d88afbb6d03f88cdbd4abc068138
lancecrowder <thelancecrowder@gmail.com>
2018-05-02 11:44:28 -0500

    Methods: Multiply recursive added from Luis' example.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -5,6 +5,7 @@ public class MethodsExercises {
         System.out.println("multiplication = " + multiplication(5, 2));
         System.out.println("division = " + division(5, 2));
         System.out.println("modulus = " + modulus(5, 2));
+        System.out.println(("multiply recursive = " + multiplyRecursive(5, 2)));
     }
 
     public static double addition (double i, double j) {
@@ -19,11 +20,25 @@ public class MethodsExercises {
         return i*j;
     }
 
-    public static double division (double i, double j) {
-        return i/j;
-    }
+    public static double division (double i, double j) { return i/j; }
 
     public static double modulus (double i, double j) {
         return i%j;
     }
+
+//    public static int getInteger(int min, int max);
+
+    public static double multiplyRecursive (double i, double j){
+        if (i == 0 || j == 0){
+            return 0;
+        }
+        if (i == 1){
+            return j;
+        }
+        if (j == 1){
+            return i;
+        }
+        return i + multiplyRecursive(i,j-1);
+    }
+
 }
