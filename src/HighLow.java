44ac020deecb4a2d92d17b9f3ef5df030ca2d834
lancecrowder <thelancecrowder@gmail.com>
2018-05-03 10:30:37 -0500

    Game Development 101 - it Works!

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class HighLow {
     public static void main(String[] args) {
-//        Game picks a random number between 1 and 100.
         int randoNum = randoNum();
-//        Prompts user to guess the number
-
-//        All user inputs are validated
-//        If user's guess is less than the number, it outputs "HIGHER"
-//        If user's guess is more than the number, it outputs "LOWER"
-//        If a user guesses the number, the game should declare "GOOD GUESS!"
+        int userGuess;
+        do {
+            userGuess = userNum();
+//          All user inputs are validated
+//           isValid(userGuess);
+            if(userGuess<randoNum){
+                System.out.println("HIGHER");
+            }else if (userGuess>randoNum){
+                System.out.println("LOWER");
+            } else {
+                System.out.println("GOOD GUESS!!!");
+            }
+        } while (userGuess != randoNum);
     }
+
+    public static int randoNum(){
+        return (int) (Math.random()*100) + 1;
+    }
+
+    public static int userNum(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Number-Guess: ");
+        return input.nextInt();
+    }
+
+//    public static int isValid(userGuess)
 }
