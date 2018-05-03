5c3e17928022f096f2f01c53b10fad6da0037127
lancecrowder <thelancecrowder@gmail.com>
2018-05-03 16:21:13 -0500

    Understanding references complete

--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,10 @@ class Person {
         name = userName;
     }
 
+    public String getName(){
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -18,5 +22,22 @@ class Person {
         user.sayHello();
         user.setName("Lance");
         user.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
