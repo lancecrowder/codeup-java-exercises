b9b2151c767aad2ddc90f757f82374637ccab6f4
lancecrowder <thelancecrowder@gmail.com>
2018-05-03 15:16:20 -0500

    Person Class created

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public static void main(String[] args) {
+        private String name (
+        // returns the person's name
+        public String getName();
+        // changes the name property to the passed value
+        public void setName(String name);
+        // prints a message to the console using the person's name
+        public void sayHello();
+    }
+    )
+}
