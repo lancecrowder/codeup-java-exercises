dfa4250db556c4d9bab6734ec2e341fc04fdb806
lancecrowder <thelancecrowder@gmail.com>
2018-05-04 14:24:21 -0500

    After Walkthrough push

--- src/Shapes/CircleApp.java
+++ src/Shapes/CircleApp.java
@@ -2,12 +2,11 @@ package Shapes;
 
 import util.Input;
 
-
 public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
-        double radius = input.getDouble();
-        Circle circle = new Circle(radius);
+//        double radius = input.getDouble();
+        Circle circle = new Circle(/* radius */input.getDouble());
         System.out.println("Your circle's Area is " + circle.getArea());
         System.out.println("Your circle's Circumference is " +circle.getCircumference());
     }
