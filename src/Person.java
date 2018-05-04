abe30a3f089fdac801460991c0550de484e2d10b
lancecrowder <thelancecrowder@gmail.com>
2018-05-04 11:08:21 -0500

    Person cleaned up

--- src/Person.java
+++ src/Person.java
@@ -23,17 +23,17 @@ class Person {
         user.setName("Lance");
         user.sayHello();
 
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
-
         Person person1 = new Person("John");
-        Person person2 = person1;
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+        person1 = new Person("John");
+        person2 = person1;
+        System.out.println(person1 == person2);
+
+        person1 = new Person("John");
+        person2 = person1;
         System.out.println(person1.getName());
         System.out.println(person2.getName());
         person2.setName("Jane");
