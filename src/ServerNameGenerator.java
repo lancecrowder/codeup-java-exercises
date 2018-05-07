fc757bb5940d3d043fe0a047f7eaaad7d819f0df
lancecrowder <thelancecrowder@gmail.com>
2018-05-07 11:09:25 -0500

    Server Name Generator Complete

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,13 @@
 public class ServerNameGenerator {
-
-    String[] adjectives = {Mammoth, Spotty, Boundless, Thoughtfull, Barbarous, Languid, Chunky, Dizzy, Unsightly, Fallacious};
-    String[] nouns = {Afterthought, Observation, Beginner, Snail, Coffee, Wind, Airport, Insurance, Morning, Arithmetic};
-
-
-
+    public static void main(String[] args) {
+        String[] adjectives = {"Mammoth", "Spotty", "Boundless", "Thoughtful", "Barbarous", "Languid", "Chunky", "Dizzy", "Unsightly", "Fallacious"};
+        String[] nouns = {"Afterthought", "Observation", "Beginner", "Snail", "Coffee", "Wind", "Airport", "Insurance", "Morning", "Arithmetic"};
+        System.out.println("Here is your server name:\n" + adjectives[adjectiveRoll()] + "-" + nouns[nounRoll()] + "!");
+    }
+    private static int adjectiveRoll() {
+        return ((int) (Math.random() * 10));
+    }
+    private static int nounRoll() {
+        return ((int) (Math.random() * 10));
+    }
 }
