de258839110f52edd83c8b457ec094bf7f516562
lancecrowder <thelancecrowder@gmail.com>
2018-05-08 13:53:31 -0500

    Square override Complete.  I checked the results by changing the math on the override then changing it back once verified.

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,7 +1,21 @@
 package Shapes;
 
 public class Square extends Rectangle {
-    public Square (int side){
+
+    protected int side;
+
+    public Square(int side) {
         super(side, side);
+        this.side = side;
     }
+
+//        @Override
+        public int getArea() {
+        return ((int)Math.pow(side, 2));
+        }
+
+        public int getPerimeter(){
+        return 4*side;
+        }
+
 }
