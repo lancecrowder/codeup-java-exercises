ca922c4d4e3c246e47b59646c7c33c87e740d597
lancecrowder <thelancecrowder@gmail.com>
2018-05-10 11:34:42 -0500

    Interfaces and Abstract Classes Exercise completed.

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -4,24 +4,24 @@ public class ShapesTest {
         Rectangle box1 = new Rectangle(4,5);
         Rectangle box2 = new Square(5);
 
-//        System.out.println("box1 length = " + box1.getLength());
-//        System.out.println("box1 width = " + box1.getWidth());
-//        System.out.println("box1 perimeter = " + box1.getPerimeter());
-//        System.out.println("box1 area = " + box1.getArea());
-//        System.out.println();
-//        System.out.println("box2 length = " + box2.getLength());
-//        System.out.println("box2 width = " + box2.getWidth());
-//        System.out.println("box2 perimeter = " + box2.getPerimeter());
-//        System.out.println("box2 area = " + box2.getArea());
-
-        Measurable myShape = box1;
+        System.out.println("box1 length = " + box1.getLength());
+        System.out.println("box1 width = " + box1.getWidth());
+        System.out.println("box1 perimeter = " + box1.getPerimeter());
+        System.out.println("box1 area = " + box1.getArea());
+        System.out.println();
+        System.out.println("box2 length = " + box2.getLength());
+        System.out.println("box2 width = " + box2.getWidth());
+        System.out.println("box2 perimeter = " + box2.getPerimeter());
+        System.out.println("box2 area = " + box2.getArea());
+        System.out.println();
+        Measurable myShape = new Rectangle(8,10);;
         System.out.println("Rectangle Stuff:");
-        System.out.println(myShape.getArea());
-        System.out.println(myShape.getPerimeter());
-
+        System.out.println("Area: " + myShape.getArea());
+        System.out.println("Perimeter: " + myShape.getPerimeter());
+        System.out.println();
         System.out.println("Box Stuff");
-        Measurable myNewShape = box2;
-        System.out.println(myNewShape.getArea());
-        System.out.println(myNewShape.getPerimeter());
+        Measurable myNewShape = new Square(10);
+        System.out.println("Area: " + myNewShape.getArea());
+        System.out.println("Perimeter: " + myNewShape.getPerimeter());
     }
 }
